c9f8d16caa3060b32bd28647742056c9db4ca53c
Allen Gooch <allen.gooch@gmail.com>
2018-06-27 19:02:08 -0500

    Cleanup some stuff

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -15,24 +15,17 @@ locals {
 # Back-end Auto-scaling Group (ASG) Resources
 #------------------------------------------------------------------------------
 
-data "template_file" "user_data" {
-  template                = "./user_data.tpl"
-
-  vars {
-    packages              = "nginx"
-  }
-}
-
 resource "aws_launch_configuration" "backend" {
   name_prefix             = "${local.cluster_name}-lc"
   image_id                = "${var.source_ami}"
   instance_type           = "${var.instance_type}"
   security_groups         = ["${aws_security_group.backend.id}"]
   key_name                = "samba@cloudserver"
-//  user_data               = "${data.template_file.user_data.rendered}"
   user_data = <<-EOF
               #!/bin/bash
-              touch /home/admin/ITWORKS
+              apt-get install nginx -y
+              # echo "Hello from `hostname`" > /var/www/html/index.nginx-debian.html
+              echo "Hello from `hostname`" > /var/www/html/index.html
               EOF
 
   lifecycle {
